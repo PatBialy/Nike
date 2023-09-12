6a3d71594f501231b4f4585b3ad5acfca00e1bd4
Patrick Bialy <100451441+PatBialy@users.noreply.github.com>
2023-09-12 14:59:33 -0400

    Hamburger Menu For Nav Mobile

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
 const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
@@ -15,26 +26,36 @@ const Nav = () => {
             className="m-0 w-[129px] h-[29px]"
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        {/* Display hamburger menu icon only on mobile screens when the menu is closed */}
+        {!isMenuOpen && (
+          <div className="lg:hidden">
+            <img
+              src={hamburger}
+              alt="hamburger icon"
+              width={25}
+              height={25}
+              onClick={toggleMenu}
+            />
+          </div>
+        )}
+        {/* Display navigation options */}
+        <ul
+          className={`lg:flex justify-center items-center gap-16 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+        >
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
                 className="font-montserrat leading-normal text-lg text-slate-gray"
+                onClick={closeMenu}
               >
                 {item.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
-          <a href="/">Sign in</a>
-          <span>/</span>
-          <a href="/">Explore now</a>
-        </div>
-        <div className="hidden max-lg:block">
-          <img src={hamburger} alt="hamburger icon" width={25} height={25} />
-        </div>
       </nav>
     </header>
   );
